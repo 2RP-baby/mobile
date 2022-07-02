332b2768402217c709e9332f8b4a86a6ca47afad
LimJiYeonn <jiueon6@gmail.com>
2022-07-02 15:44:36 +0900

    input

--- src/utils/context.js
+++ src/utils/context.js
@@ -3,11 +3,11 @@ import React, { createContext } from 'react';
 
 import createRootStore from '../store/index';
 
-
 export const storesContext = createContext(null);
 
 export const StoreProvider = ({ children }) => {
   const store = useLocalObservable(createRootStore);
+  console.log(3);
 
   return <storesContext.Provider value={store}>{children}</storesContext.Provider>;
 };
