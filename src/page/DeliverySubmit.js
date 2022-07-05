a2a27aa9883b14273801fe78b785744d71f2a156
LimJiYeonn <jiueon6@gmail.com>
2022-07-05 14:18:29 +0900

    db insert

--- src/page/DeliverySubmit.js
+++ src/page/DeliverySubmit.js
@@ -1,20 +1,64 @@
 import React, { useState } from 'react';
 import { View, StyleSheet, Button, Alert, TextInput, Text} from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { insertSccDeliveryInfo } from '../apis/scc';
 import FixBox from '../component/deliveryInsert/FixBox';
 import InputInfo from '../component/deliverySubmit/InputInfo';
 import ItemInfo from '../component/deliverySubmit/ItemInfo';
+import useRootData from '../hooks/useRootData';
 
 const DeliverySubmit = ({navigation}) => {
-    const [deliveryConditon, setDeliveryConditon] = useState([]);
+
+    const {
+        // sccSearchOne의 결과
+        deliveryInsertInfo,
+        changeDeliveryInsertInfo,
+
+        // DeliveryInsert page에서 넘어온 list
+        checkedList,
+        changeCheckedList,
+    } = useRootData(({deliveryInsertStore, checkedListStore}) => ({
+        deliveryInsertInfo: deliveryInsertStore.deliveryInsertInfo.get(),
+        changeDeliveryInsertInfo: deliveryInsertStore.changeDeliveryInsertInfo,
+        checkedList: checkedListStore.checkedList.get(),
+        changeCheckedList: checkedListStore.changeCheckedList,
+    }));
+
+    // checkedList : json -> 배열
+    console.log("checkedList :!!!!~~~", Object.values(checkedList));
+    const scc2List = Object.values(checkedList);
+    
+    const [inputData, setInputData] = useState({
+        "scc1" : {
+            "vendor_site_id" : deliveryInsertInfo[0].vendor_id,
+            "employee_number" : 1685,  	                        //로그인한 사람(staffId)
+            "deliver_to_location" : "",	                        // input
+            "comment" : "",  	                                // input
+            "po_header_id" : deliveryInsertInfo[0].po_header_id,
+            "po_release_id" : "",                               // 일단 비워두기
+            "subniventory" : "",                                // input
+        },
+
+        "scc2List" : scc2List,
+    });
 
     const handleDeliveryCondition = (key, value) => {
-        const tempCondition = { ...deliveryConditon };
-        tempCondition[key] = value;
-        setDeliveryConditon(tempCondition);
+        const tempCondition = { ...inputData };
+        tempCondition["scc1"][key] = value;
+        setInputData(tempCondition);
     };
-    console.log("deliveryConditon", deliveryConditon);
 
-    const goAlert = () =>
+    console.log("inputData : ", inputData);
+    // console.log("deliveryInsertInfo : ", deliveryInsertInfo);
+
+
+    // 납품신청 버튼
+    const InsertInfo =  async (sendData) => {
+        const data = await insertSccDeliveryInfo(sendData);
+        console.log(data);
+    };  
+
+    const goAlert = () =>{
         Alert.alert(                   
         "납품 신청을 하시겠습니까?",                 // 첫번째 text: 타이틀 제목
         "신청 완료 후 메뉴 페이지로 이동합니다. ",   // 두번째 text: 그 밑에 작은 제목
@@ -24,24 +68,28 @@ const DeliverySubmit = ({navigation}) => {
                 onPress: () => console.log("아니라는데"),     //onPress 이벤트시 콘솔창에 로그를 찍는다
                 style: "cancel"
             },
-            { text: "네", onPress: () => navigation.navigate('Menu') }, //버튼 제목
+            { text: "네", onPress: () => {
+                navigation.navigate('Menu');
+                InsertInfo(inputData);
+            }}, //버튼 제목
         ],
         { cancelable: false }
         );  
-
+    }
     return (
+        <ScrollView>
         <View style={styles.header}>
             <FixBox/>
             <ItemInfo/>
             <InputInfo id="deliver_to_location" labelContext="납품장소" handleCondition={handleDeliveryCondition}/>
             <InputInfo id="comment" labelContext="요청 특기사항" handleCondition={handleDeliveryCondition}/>
-            <InputInfo id="po_header_id" labelContext="신청부서 (Code 입력)" handleCondition={handleDeliveryCondition}/>
+            <InputInfo id="subniventory" labelContext="신청부서 (Code 입력)" handleCondition={handleDeliveryCondition}/>
             <View style={styles.button}>
                 <Button title="납품신청" color="#005386"
                     onPress={() => goAlert()}/>
             </View>
-
         </View>
+        </ScrollView>
     );
 };
 
