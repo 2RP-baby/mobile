634e88bd7300da1fbb690c922d677dfb6286689d
LimJiYeonn <jiueon6@gmail.com>
2022-07-12 17:51:47 +0900

    draw bar

--- src/component/CustomDrawer.js
+++ src/component/CustomDrawer.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {View, Text, Button, StyleSheet, Image, ImageBackground} from 'react-native';
+import {View, Text, Button, StyleSheet, Image, ImageBackground, ScrollView} from 'react-native';
 import { DrawerContentScrollView, DrawerItem, DrawerItemList } from '@react-navigation/drawer';
 // import Ionicons from 'react-native-vector-icons/Ionicons';
 // import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import {TouchableOpacity } from 'react-native-gesture-handler';
 import logoutIcon from '../../assets/icon/logout.png'
 
 const CustomDrawer = (props) => {
@@ -13,17 +13,13 @@ const CustomDrawer = (props) => {
                     {/* <ImageBackground 
                         source ={require('../../assets/image/POSCO_ICT_CI_ENG.png')}
                         style={{padding:20}}/> */}
-                        
-                    <Image 
+                    <View style = {{flexDirection:'column' }}>
+                        <Image 
                         source={require('../../assets/icon/profile.jpg')} 
-                        style={{height:80,width:80,borderRadius:40, marginBottom:10, marginLeft:20}}
-                    />
-                    <Text style={{color:'#fff',fontSize: 18, marginLeft:10 , marginTop:10, marginBottom:10}}>이윤성 프로님</Text>
-                    <View style = {{flexDirection:'row'}}>
+                        style={{height:80,width:80,borderRadius:40, marginBottom:10, marginLeft:20}}/>
+                        <Text style={{color:'#fff',fontSize: 18, marginLeft:10 , marginTop:10, marginBottom:10}}>이윤성 프로님</Text>
                         <Text style={{color:'#fff',fontSize: 12, marginLeft:10, marginBottom:15}}>ERP 사업부</Text>
                         {/* <FontAwesome5 name = "coins" size={12} color="#fff" marginLeft={10}></FontAwesome5> */}
-                    </View>
-                    <View style = {{flex:1, backgroundColor:'#fff', paddingTop:10}}>
                         <DrawerItemList {...props}/>
                     </View>
             </DrawerContentScrollView>
@@ -43,5 +39,6 @@ const styles = StyleSheet.create({
         width: 15,
         height: 15,
     },
+
 })
 export default CustomDrawer;
